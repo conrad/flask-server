f43141319abcb5d00757f37bbc36e3fb32ddd07e
Matt Conrad <mmconrad@gmail.com>
2016-11-12 09:59:58 -0800

    Add initial templates with inheritance and control logic (Jinja2); Add README.md

--- app/views.py
+++ app/views.py
@@ -1,6 +1,27 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
+def hello():
+    return "Hello, World!"
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'nickname': 'MC'}
+    posts = [
+        {
+            'author': {'nickname': 'John'},
+            'body': 'Beautiful day in SF!'
+        },
+        {
+            'author': {'nickname': 'Susan'},
+            'body': 'The Cumberbatch movie was so cool!'
+        },
+        {
+            'author': {'nickname': 'Clark'},
+            'body': 'Let\'s go for a sail!'
+        }
+    ]
+    return render_template('index.html',
+                            title='Home',
+                            user=user,
+                            posts=posts)
