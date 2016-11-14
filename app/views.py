6dea2f44083a9e6f2c55fc80d00c5e945fdabdf7
Matt Conrad <mmconrad@gmail.com>
2016-11-13 21:15:36 -0800

    Add validation error messages and links for Open ID providers

--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template
+from flask import render_template, redirect, flash
 from .forms import LoginForm
 
 @app.route('/')
@@ -31,6 +31,11 @@ def index():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested fro OpenID="%s", remember_me=%s' %
+            (form.openid.data, str(form.remember_me.data)))
+        return redirect('/index')
     return render_template('login.html',
                             title='Sign In',
-                            form=form)
+                            form=form
+                            providers=app.config['OPENID_PROVIDERS'])
